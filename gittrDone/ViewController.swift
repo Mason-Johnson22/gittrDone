31e6cbd0a252b77f9bedf5b7626417d278fb5829
Mason Johnson <masonalexanderjohnson@Masons-MacBook-Air.local>
2019-10-28 14:40:01 -0400

    Initial Commit

---
+++ gittrDone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittrDone
+//
+//  Created by Mason  Alexander Johnson  on 10/28/19.
+//  Copyright © 2019 Mason  Alexander Johnson . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

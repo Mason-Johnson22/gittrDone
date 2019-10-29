e909eb288beff39dc219cf07b33c88c1d5e5adf0
Mason Johnson <masonalexanderjohnson@Masons-MacBook-Air.local>
2019-10-29 14:15:57 -0400

    bracket

--- gittrDone/ViewController.swift
+++ gittrDone/ViewController.swift
@@ -19,10 +19,9 @@ class ViewController: UIViewController {
     
     let larry = UIImage(named: "Larry")
     
-    }
     func heyDeath() {
         print("Hi, concerned dad here. While browsing the web with my son, who is aged 9, i came across this ad for this fortnite,and my son started screaming 'papa please download it is good game!!!!' After downloading it, i was immediatly flooded with ads and porn, and had to reinstall windows. After that though, i got the official download and my son started playing it. Soon, i started hearing strange voices coming from the monitor whenever i let him play. Some of the things they said was 'Come the frick over here and revive me you fricker!' I also heard little kids arguing with there parents and this one women accusing this guy of this thing called 'rope'. Whatever, I stray from the point. After awhile, he started doing these strange dances that perplexed be, and started calling me a 'noobmaster69' and a 'default'. As a concerned parent, I do not know what to do, he has also started begging me for this 'battlepass' and when i said no he started doing this dance called the floss and told me that I am not a good builder. Reddit, Does My son have a certain Behavorial issue? If so, where should i take him to check his behavior?")
     }
 
-
+} 
 
